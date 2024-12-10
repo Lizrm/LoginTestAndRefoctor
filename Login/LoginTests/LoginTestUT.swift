a594778e7e826de60b9571697493d7d3cd875522
Lizbeth <20135719+Lizrm@users.noreply.github.com>
2024-12-10 15:43:07 -0600

    Add files via upload

--- Login/LoginTests/LoginTestUT.swift
+++ Login/LoginTests/LoginTestUT.swift
@@ -28,25 +28,40 @@ class LoginTestUT: XCTestCase {
         super.tearDown()
     }
     
-    
+    /// Valida que el usuaio No se encuentra Registrado
+    /// - Given details: Datos de usuairo invalidos
+    /// - AssertFalse: Bool false  que el usuario no esta registrado
     func testNotRegisterUSerAndInvalidEmail() {
      
         let email =  "Lisbeth12@gmail.com"
         let password =  "Lisbeth812gmail"
         XCTAssertFalse(LoginValidator.authenticateUser(email, password))
     }
-    
+    /// Valida que el password y usuario(email) sean los correctos
+    /// - Given details: email correcto, password correcto
+    /// - AssertTrue: Bool True el usuario se encuentra registrado y el password es correcto
     func testValidUSerRegister() {
   
         let email =  "Lisbeth812@gmail.com"
         let password =  "Lisbeth812gmail"
         XCTAssertTrue(LoginValidator.authenticateUser(email, password))
     }
-    
+    /// Valida que el password y usuario(email) sean los correctos
+    /// - Given details: email correcto, password incorrecto
+    /// - AssertFalse: Bool false el password es incorrecto
     func testInvalidPassword() {
   
         let email =  "Lisbeth812@gmail.com"
         let password =  "Lisbeth812ail"
+        XCTAssertFalse(LoginValidator.authenticateUser(email, password))
+    }
+    /// Valida que el password y usuario(email) sean los correctos
+    /// - Given details: email vacio, password vacio
+    /// - AssertFalse: Bool data para login vacio
+    func testEmptyData() {
+  
+        let email =  ""
+        let password =  ""
         XCTAssertFalse(LoginValidator.authenticateUser(email, password))
     }
     
--- Login/LoginTests/RegistrationUserUT.swift
+++ Login/LoginTests/RegistrationUserUT.swift
@@ -21,7 +21,9 @@ class RegistrationUserUT: XCTestCase {
         super.tearDown()
     }
     
-    
+    /// Valida que el usuaio sea creado
+    /// - Given details: datos de usuaio correctos
+    /// - Assert: Bool true que el usuario fue creado
     func testRegisterUSerValidData() {
         var details: UserRegistrationDetails
         details = UserRegistrationDetails(firstName: "Lisbeth", lastName: "Garcia", email: "Lisbeth812@gmail.com", age: 21, password: "Lisbeth812gmail")
@@ -29,6 +31,9 @@ class RegistrationUserUT: XCTestCase {
         XCTAssertTrue(userValidator.registerUser(details))
     }
     
+    /// Valida que el usuaio No fue  creado
+    /// - Given details: Dato de email incorrecto
+    /// - AssertFalse: Bool false que el usuario NO fue creado
     func testRegisterUSerInvalidEmail() {
         var details: UserRegistrationDetails
         details = UserRegistrationDetails(firstName: "Lisbeth", lastName: "Garcia", email: "Lisbeth812mail.com", age: 21, password: "Lisbeth812gmail")
@@ -36,37 +41,54 @@ class RegistrationUserUT: XCTestCase {
         XCTAssertFalse(userValidator.registerUser(details))
     
     }
-    
+    /// Valida que el usuaio No fue  creado
+    /// - Given details: Dato de age invalido  menor a 18
+    /// - AssertFalse: Bool false que el usuario NO fue creado
     func testRegisterUSerInvalidAge() {
         var details: UserRegistrationDetails
-        details = UserRegistrationDetails(firstName: "Lisbeth", lastName: "Garcia", email: "Lisbeth812mail.com", age: 21, password: "Lisbeth812gmail")
+        details = UserRegistrationDetails(firstName: "Lisbeth", lastName: "Garcia", email: "Lisbeth812@gmail.com", age: 9, password: "Lisbeth812gmail")
         
         XCTAssertFalse(userValidator.registerUser(details))
     
     }
-    
+    /// Valida que el usuaio No fue  creado
+    /// - Given details: Dato de password incorrecto  menos de 6 caracteres
+    /// - AssertFalse: Bool false que el usuario NO fue creado
     func testRegisterUSerInvalidPassword() {
         var details: UserRegistrationDetails
-        details = UserRegistrationDetails(firstName: "Lisbeth", lastName: "Garcia", email: "Lisbeth812mail.com", age: 21, password: "Lis")
+        details = UserRegistrationDetails(firstName: "Lisbeth", lastName: "Garcia", email: "Lisbeth812@gmail.com", age: 21, password: "Lis")
         
         XCTAssertFalse(userValidator.registerUser(details))
     
     }
-    
+    /// Valida que el usuaio No fue  creado
+    /// - Given details: Dato de firstName vacio
+    /// - AssertFalse: Bool false que el usuario NO fue creado
     func testRegisterUSerEmptyFirtsName() {
         var details: UserRegistrationDetails
-        details = UserRegistrationDetails(firstName: "", lastName: "Garcia", email: "Lisbeth812mail.com", age: 21, password: "Lis")
+        details = UserRegistrationDetails(firstName: "", lastName: "Garcia", email: "Lisbeth812@gmail.com", age: 21, password: "Lisbeth812gmail")
         
         XCTAssertFalse(userValidator.registerUser(details))
     
     }
-    
+    /// Valida que el usuaio No fue  creado
+    /// - Given details: Dato de lastName vacio
+    /// - AssertFalse: Bool false que el usuario NO fue creado
     func testRegisterUSerEmptyLastName() {
         var details: UserRegistrationDetails
-        details = UserRegistrationDetails(firstName: "Lisbeth", lastName: "", email: "Lisbeth812mail.com", age: 21, password: "Lis")
+        details = UserRegistrationDetails(firstName: "Lisbeth", lastName: "", email: "Lisbeth812@gmail.com", age: 21, password: "Lisbeth812gmail")
         
         XCTAssertFalse(userValidator.registerUser(details))
     
     }
+    /// Valida que el usuaio No fue  creado
+    /// - Given details: Datos de usuairo vacios
+    /// - AssertFalse: Bool false que el usuario NO fue creado
+    func testRegisterUSerEmptyData() {
+        var details: UserRegistrationDetails
+        details = UserRegistrationDetails(firstName: "", lastName: "", email: "", age: 0, password: "")
+        
+        XCTAssertFalse(userValidator.registerUser(details))
     
+    }
 }
